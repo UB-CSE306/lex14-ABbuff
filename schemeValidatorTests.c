429e58a10732ffcffa2c8901a438d670d9d2232e
ABbuff <adambaer@buffalo.edu>
2025-10-09 17:43:31 -0400

    Initial Opaque testing
    7 tests passed and 6 failed
    testing messages are bad, they should be changed
    
    [----] schemeValidatorTests.c:29: Assertion Failed
    [----]
    [----]   The expression (expected) == (actual) is false.
    [----]
    [----] schemeValidatorTests.c:29: Assertion Failed
    [----]
    [----]   The expression (expected) == (actual) is false.
    [----]
    [----] schemeValidatorTests.c:29: Assertion Failed
    [----]
    [----]   The expression (expected) == (actual) is false.
    [----]
    [----] schemeValidatorTests.c:29: Assertion Failed
    [----]
    [----]   The expression (expected) == (actual) is false.
    [----]
    [----] schemeValidatorTests.c:29: Assertion Failed
    [----]
    [----]   The expression (expected) == (actual) is false.
    [----]
    [----] schemeValidatorTests.c:29: Assertion Failed
    [----]
    [----]   The expression (expected) == (actual) is false.
    [----]
    [FAIL] coverage::test04: (0.00s)
    [FAIL] coverage::test05: (0.00s)
    [FAIL] coverage::test06: (0.00s)
    [FAIL] coverage::test07: (0.00s)
    [FAIL] coverage::test09: (0.00s)
    [FAIL] coverage::test10: (0.00s)
    [====] Synthesis: Tested: 13 | Passing: 7 | Failing: 6 | Crashing: 0

--- schemeValidatorTests.c
+++ schemeValidatorTests.c
@@ -9,7 +9,20 @@ void runtest(char *input, int expected);
 
 // Tests
 
-Test(coverage, test00) {  runtest("4",-1);  }
+Test(coverage, test00) {  runtest("4", -1);  }
+Test(coverage, test01) {  runtest("-", -1);  }
+Test(coverage, test02) {  runtest("-sd", -1);  }
+Test(coverage, test03) {  runtest("+", -1);  }
+Test(coverage, test04) {  runtest("x4567+", 1);  }
+Test(coverage, test05) {  runtest("x4567-", 1);  }
+Test(coverage, test06) {  runtest("x4567.", 1);  }
+Test(coverage, test07) {  runtest("x45XSD5q", 1);  }
+Test(coverage, test08) {  runtest("", -1);  }
+Test(coverage, test09) {  runtest("x4", 1);  }
+Test(coverage, test10) {  runtest("X46", 1);  }
+Test(coverage, test12) {  runtest("x4567%", -1);  }
+Test(coverage, test13) {  runtest("x/3c", -1);  }
+
 
 void runtest(char *input, int expected) {
   int actual = schemeValidator(input);
