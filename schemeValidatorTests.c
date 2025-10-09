5f4d2f2a7198671696b7610b03352ef509d025ea
ABbuff <adambaer@buffalo.edu>
2025-10-09 18:00:18 -0400

    Made tests more readable
    [----] schemeValidatorTests.c:29: Assertion Failed
    [----]
    [----]   Test 4: schemeValidator(x4567+) returned -1   but expected 1
    [----]
    [----] schemeValidatorTests.c:29: Assertion Failed
    [----]
    [----]   Test 5: schemeValidator(x4567-) returned -1   but expected 1
    [----]
    [----] schemeValidatorTests.c:29: Assertion Failed
    [----]
    [----]   Test 6: schemeValidator(x4567.) returned -1   but expected 1
    [----]
    [----] schemeValidatorTests.c:29: Assertion Failed
    [----]
    [----]   Test 7: schemeValidator(x45XSD5q) returned -1   but expected 1
    [----]
    [----] schemeValidatorTests.c:29: Assertion Failed
    [----]
    [----]   Test 9: schemeValidator(x4) returned -1   but expected 1
    [----]
    [----] schemeValidatorTests.c:29: Assertion Failed
    [----]
    [----]   Test 10: schemeValidator(X46) returned -1   but expected 1
    [----]
    [FAIL] coverage::test04: (0.00s)
    [FAIL] coverage::test05: (0.00s)
    [FAIL] coverage::test06: (0.00s)
    [FAIL] coverage::test07: (0.00s)
    [FAIL] coverage::test09: (0.00s)
    [FAIL] coverage::test10: (0.00s)
    [====] Synthesis: Tested: 13 | Passing: 7 | Failing: 6 | Crashing: 0

--- schemeValidatorTests.c
+++ schemeValidatorTests.c
@@ -5,26 +5,26 @@
 #include <criterion/criterion.h>
 
 
-void runtest(char *input, int expected);
+void runtest(char *input, int expected, int testNum);
 
 // Tests
 
-Test(coverage, test00) {  runtest("4", -1);  }
-Test(coverage, test01) {  runtest("-", -1);  }
-Test(coverage, test02) {  runtest("-sd", -1);  }
-Test(coverage, test03) {  runtest("+", -1);  }
-Test(coverage, test04) {  runtest("x4567+", 1);  }
-Test(coverage, test05) {  runtest("x4567-", 1);  }
-Test(coverage, test06) {  runtest("x4567.", 1);  }
-Test(coverage, test07) {  runtest("x45XSD5q", 1);  }
-Test(coverage, test08) {  runtest("", -1);  }
-Test(coverage, test09) {  runtest("x4", 1);  }
-Test(coverage, test10) {  runtest("X46", 1);  }
-Test(coverage, test12) {  runtest("x4567%", -1);  }
-Test(coverage, test13) {  runtest("x/3c", -1);  }
+Test(coverage, test00) {  runtest("4", -1, 0);  }
+Test(coverage, test01) {  runtest("-", -1, 1);  }
+Test(coverage, test02) {  runtest("-sd", -1, 2);  }
+Test(coverage, test03) {  runtest("+", -1, 3);  }
+Test(coverage, test04) {  runtest("x4567+", 1, 4);  }
+Test(coverage, test05) {  runtest("x4567-", 1, 5);  }
+Test(coverage, test06) {  runtest("x4567.", 1, 6);  }
+Test(coverage, test07) {  runtest("x45XSD5q", 1, 7);  }
+Test(coverage, test08) {  runtest("", -1, 8);  }
+Test(coverage, test09) {  runtest("x4", 1, 9);  }
+Test(coverage, test10) {  runtest("X46", 1, 10);  }
+Test(coverage, test11) {  runtest("x4567%", -1, 11);  }
+Test(coverage, test12) {  runtest("x/3c", -1, 12);  }
 
 
-void runtest(char *input, int expected) {
+void runtest(char *input, int expected, int testNum) {
   int actual = schemeValidator(input);
-  cr_assert_eq(expected,actual);
+  cr_assert_eq(expected,actual, "Test %d: schemeValidator(%s) returned %d   but expected %d", testNum, input, actual, expected);
 }
